d2d8be07c923711874b6b7399fe1a220d906c0e1
nusanetanc <anc.nusanet@gmail.com>
2016-03-24 15:13:58 +0700

    Update index.php
    penambahan widget tawk.to

--- index.php
+++ index.php
@@ -28,4 +28,16 @@ if (!isset($_SESSION["id"])) {
 	<script type="" language="JavaScript">
 	document.location='./member/'</script>
 <?php } ?>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/56f39d18f49b1f546d34d638/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
