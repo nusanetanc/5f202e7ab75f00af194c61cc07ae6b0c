c00be02faa48c92977b5b25b0b9b16375aff4347
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-03-24 10:06:03 +0700

    perbaikan kirim email akun staff

--- member/payment.php
+++ member/payment.php
@@ -13,26 +13,28 @@
 						  <thead>
 						    <tr>
 						      <th width="10%">ID Cust</th>
-						      <th width="40%">Customer</th>
-						      <th width="20%">Paket</th>
-						      <th width="20%">Tgl Registrasi</th>
+						      <th width="30%">Customer</th>
+						      <th width="20">Status</th>
+						      <th width="15%">Paket</th>
+						      <th width="15%">Tgl Registrasi</th>
 						      <th width="10%">Detail</th>
 						    </tr>
 						  </thead>
 						  <?php
 					$res = $col_user->find(array("level"=>"0"));
 					foreach($res as $row) 
-                      {  if($row['status']=="konfirmasi registrasi" || $row['status']=="aktif") { ?>
+                      { ?>
 						  <tbody>
 						    <tr>
 						      <td><?php echo $row['id_user']; ?></td>
 						      <td><?php echo $row['nama'].' / '. $row['phone'].' / '.$row['email']; ?></td>
+						      <td><?php echo $row['status']; ?></td>
 						      <td><?php echo $row['paket']; ?></td>
 						      <td><?php echo $row['tanggal_registrasi']; ?></td>
 						      <td><b><a href="<?php echo $base_url_member; ?>/?hal=verification-payment&id_cust=<?php echo $row['id_user']; ?>" class="btn btn-primary btn-xs">Show</a></b></td>						      
 						    </tr>
 						   </tbody>
-					<?php } } ?>	   
+					<?php } ?>	   
 						</table>    
 					</div>	
  				</div>
