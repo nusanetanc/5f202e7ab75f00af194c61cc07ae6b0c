97433ddfe05c2cd4e563bbde7691a697f714e1d5
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-03-28 20:04:44 +0700

    perbaikan registrasi sales

--- member/registration-add.php
+++ member/registration-add.php
@@ -1,7 +1,11 @@
  <form enctype="multipart/form-data" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
 <?php
  echo yudi0;
- 
+  $res = $col_user->find(array("id_user"=>$sm));
+  foreach($res as $row)
+                      { 
+                          $email_sm = $row['email'];
+                        }
 echo yudi1;
 if (isset($_POST['register'])){
                               $regisname= $_POST['regisname'];
@@ -15,72 +19,7 @@ if (isset($_POST['register'])){
                               $date_years = date("y");
                               $date_days = date("d");  
                               $bulan1 = bulan($date_month);
-                //membuat id user
-                /**
-                 * Luhn algorithm
-                 *
-                 * The Luhn algorithm or Luhn formula, also known as the "modulus 10" or
-                 * "mod 10" algorithm, is a simple checksum formula used to validate a
-                 * variety of identification numbers, such as credit card numbers,
-                 * IMEI numbers, National Provider Identifier numbers in the US, and
-                 * Canadian Social Insurance Numbers. It was created by IBM scientist
-                 * Hans Peter Luhn and described in U.S. Patent No. 2,950,048, filed
-                 * on January 6, 1954, and granted on August 23, 1960.
-                 *
-                 * @author odan
-                 * @copyright 2015-2016 odan
-                 * @license http://opensource.org/licenses/MIT The MIT License (MIT)
-                 * @link https://github.com/odan/luhn
-                 */
-                class userId
-                {
-                    /**
-                     * Returns the luhn check digit
-                     *
-                     * @param string $s numbers as string
-                     * @return int checksum digit
-                     */
-                    public function baru(){
-                        $six_digit_random_number = mt_rand(000001, 999999);
-                        $s=new userId;
-                        $userid = $s->create($six_digit_random_number);
-                        return $six_digit_random_number.$userid;
-                    }
-                    public function create($s)
-                    {
-                        // Add a zero check digit
-                        $s = $s . '0';
-                        $sum = 0;
-                        // Find the last character
-                        $i = strlen($s);
-                        $odd_length = $i % 2;
-                        // Iterate all digits backwards
-                        while ($i-- > 0) {
-                            // Add the current digit
-                            $sum+=$s[$i];
-                            // If the digit is even, add it again. Adjust for digits 10+ by subtracting 9.
-                            ($odd_length == ($i % 2)) ? ($s[$i] > 4) ? ($sum+=($s[$i] - 9)) : ($sum+=$s[$i]) : false;
-                        }
-                        return (10 - ($sum % 10)) % 10;
-                    }
-                    public function validate($number)
-                    {
-                        $sum = 0;
-                        $numDigits = strlen($number) - 1;
-                        $parity = $numDigits % 2;
-                        for ($i = $numDigits; $i >= 0; $i--) {
-                            $digit = substr($number, $i, 1);
-                            if (!$parity == ($i % 2)) {
-                                $digit <<= 1;
-                            }
-                            $digit = ($digit > 9) ? ($digit - 9) : $digit;
-                            $sum += $digit;
-                        }
-                        return (0 == ($sum % 10));
-                    }
-                }
-                $userid=new userId();
-                $newid=$userid->baru();
+
 echo yudi2;
         if ($name=="" || $email=="" || $phone=="" || $package=="-- Select Package --" || $location=="-- Location --" || $decription==""){
                                                    echo '<p class="text-danger">Registration Failed, Please Try Again!</p>';
