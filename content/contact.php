3871fa4428f4999580ba7bf0d59fe21bb025a9e2
purwanto1337 <purwanto1337@gmail.com>
2016-03-30 07:34:33 +0000

    perubahan contact

--- content/contact.php
+++ content/contact.php
@@ -1,1 +1,14 @@
-test
+<!-- Contact -->
+<div style="background:url(<?php echo $base_url; ?>/img/background-contact-us.jpg);height:500px;background-position:center;margin-top:40px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12"><img width="100%" src="<?php echo $base_url; ?>/img/background-feature-tv.png"/></div>
+        
+            <div class="col-sm-12 text-center">
+                <h2 class="mainFeatureHeading">All entertainments in one product</h2>
+                <p class="featureParagraph">Stream live TV throughout your home using many of your favorite devices with 94 + Local & International Channel. Explore interesting features and lineups while away from home on your smart phone, tablet or laptop.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Contact --> 
