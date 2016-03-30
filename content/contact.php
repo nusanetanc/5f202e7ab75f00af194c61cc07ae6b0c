ddbbe7b18d39fbdb44285dc791ddbe88f21712cc
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-03-30 17:33:03 +0700

    php untuk contact us

--- content/contact.php
+++ content/contact.php
@@ -23,8 +23,8 @@
                     <a href="" target="_blank"><i style="color:#bb0000" class="fa fa-youtube-play fa-2x footer-youtube"></i></a>
                 </p>
             </div>
-            <div class="col-sm-12 col-md-6">
-                <h5 style="font-size:17px;color:#888;margin-top:40px;margin-bottom:15px;">CONTACT FORM</h5>
+            <div class="col-sm-12 col-md-6">                  
+                <h5 style="font-size:17px;color:#777;margin-top:40px;margin-bottom:15px;">CONTACT FORM</h5>
                 <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" style="margin-top:30px;margin-bottom:100px;">
                     <?php
                         if (isset($post['send'])){
@@ -32,6 +32,7 @@
                             $email = $_POST['email'];
                             $subject = $_POST['subject'];
                             $message = $_POST['message'];
+                        if ($name<>"" || $email<>"" || $subject<>"" || $message<>""){
                         $insert=$col_contactus->insert(array("name"=>$name, "email"=>$email, "subject"=>$subject, "message"=>$message));
                                             $to1 = $email;
 
@@ -63,24 +64,21 @@
 
                                           // Mail it
                                           $kirimemail1 = mail($to1, $subject1, $message1, $headers1);
-                        }
+                                $_SESSION['message-sent']="Message Sent";
+                        } else {
+                                $_SESSION['message-sent']="Message Not Sent";
+                        } }
                     ?>
-                    <input name ="name" id = "name" style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Name" type="text" class="form-control">
-                    <input name ="email" id = "email" style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Email" type="email" class="form-control">
-                    <input name ="subject" id = "subject" style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Subject" type="text" class="form-control">
-                    <textarea name ="message" id = "message" rows="5" class="form-control" style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:100px;width:100%;" placeholder="Message"></textarea>
-                    <input id="send" name="send" type="submit" style=";text-align:center;background-color:#FF3D23;border:0px;color:#fff;height:40px;padding:0 40px 0 40px;border-radius:3px;font-weight:bold;" value="SEND"/>                    
-                <h5 style="font-size:17px;color:#777;margin-top:40px;margin-bottom:15px;">CONTACT FORM</h5>
-                <form style="margin-top:30px;margin-bottom:100px;">
-                    <input style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Name" type="text" class="form-control">
-                    <input style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Email" type="email" class="form-control">
-                    <input style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Subject" type="text" class="form-control">
-                    <textarea name ="alamat" id = "alamat" rows="5" class="form-control" style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:100px;width:100%;" placeholder="Message"></textarea>
-                    <input id="register" type="submit" style=";text-align:center;background-color:#FF3D23;border:0px;color:#fff;height:40px;padding:0 40px 0 40px;border-radius:3px;font-weight:bold;" value="SEND"/>
-                    
+                    <input name ="name" id = "name" style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Name" type="text" class="form-control" required>
+                    <input name ="email" id = "email" style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Email" type="email" class="form-control" required>
+                    <input name ="subject" id = "subject" style="background-color:rgba(255, 255, 255, 1);margin-bottom:9px;height:40px" placeholder="Subject" type="text" class="form-control" require>
+                    <textarea name ="message" id = "message" rows="5" class="form-control" style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:100px;width:100%;" placeholder="Message" require></textarea>
+                    <input id="send" name="send" type="submit" style=";text-align:center;background-color:#FF3D23;border:0px;color:#fff;height:40px;padding:0 40px 0 40px;border-radius:3px;font-weight:bold;" value="SEND"/>  
+                    <?php if(isset($_SESSION['message-sent'])) { ?>
                     <!-- Notification -->
-                    <span style="margin-left:20px;">Message Sent / <i>Pesan Telah Terkirim</i></span>
+                    <span style="margin-left:20px;"><?php echo $_SESSION['message-sent']; ?></span>
                     <!-- /Notification -->
+                    <?php unset($_SESSION["message-sent"]);  } ?>
                 </form>
             </div>
             
