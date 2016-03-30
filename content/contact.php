781c2fe5c423412a50ea5fa6395bb19192685f8e
purwanto1337 <purwanto1337@gmail.com>
2016-03-30 07:39:57 +0000

    perubahan contact

--- content/contact.php
+++ content/contact.php
@@ -1,13 +1,20 @@
 <!-- Contact -->
-<div style="background:url(<?php echo $base_url; ?>/img/background-contact-us.jpg);height:500px;background-position:center;margin-top:40px;">
+<div style="background:url(<?php echo $base_url; ?>/img/background-contact-us.jpg);background-position:center;margin-top:40px;padding-top:100px;">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12"><img width="100%" src="<?php echo $base_url; ?>/img/background-feature-tv.png"/></div>
-        
             <div class="col-sm-12 text-center">
-                <h2 class="mainFeatureHeading">All entertainments in one product</h2>
-                <p class="featureParagraph">Stream live TV throughout your home using many of your favorite devices with 94 + Local & International Channel. Explore interesting features and lineups while away from home on your smart phone, tablet or laptop.</p>
+                <h2 class="mainFeatureHeading">Contact Us</h2>
+                <p class="featureParagraph">Here are the ways you can contact us with any questions you have about groovy.</p>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                Our Office
+            </div>
+            <div class="col-sm-12 col-md-6">
+                Contact Form
+            </div>
+            
         </div>
     </div>
 </div>
