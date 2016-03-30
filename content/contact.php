8c01535b8c62d2ac84e0100ab08f70e3d5d2742b
purwanto1337 <purwanto1337@gmail.com>
2016-03-30 09:25:49 +0000

    perubahan contact us page

--- content/contact.php
+++ content/contact.php
@@ -1,29 +1,41 @@
 <!-- Contact -->
-<div style="background:url(<?php echo $base_url; ?>/img/background-contact-us.jpg);background-position:center;margin-top:40px;padding-top:50px;">
+<div style="background:url(<?php echo $base_url;?>/img/background-contact-us-black.jpg);background-position:center;margin-top:50px;padding-top:80px;">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 text-center">
-                <h2 class="mainFeatureHeading">Contact Us</h2>
-                <p class="featureParagraph">Here are the ways you can contact us with any questions you have about groovy.</p>
+            <div class="col-sm-12">
+                <h3 style="margin:0;font-size:33px;font-weight:bold;color:#fff;">Contact Us</h3>
+                <h6 style="font-size:18px;font-weight:400;line-height:24px;margin:25px 0 0 0;color:#fff">Here are the ways you can contact us with any questions you have about groovy.</h6>
+                <div style="background: linear-gradient(to right, #FF3D23 , #FF931E);height:2px;margin-top:30px;margin-bottom:5px;"></div>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-12 col-md-6">
-                <h3>Our Office</h3>
-                <p>Cyber Building 7th Floor<br/>
+                <h5 style="font-size:17px;color:#888;margin-top:40px;margin-bottom:15px;">OUR OFFICE</h5>
+                
+                <p style="margin-top:27px;font-size:15px;color:#fff;font-weight:light;line-height:1.5">Nusanet Office<br/>Cyber Building, 7th Floor<br/>
                 Jl. Kuningan Barat 8<br/>
                 Jakarta 12710, Indonesia</p>
+                <h5 style="font-size:17px;color:#888;margin-top:40px;margin-bottom:15px;">SOCIAL MEDIA</h5>
+                <p style="line-height:25px;">
+                    <a href="" target="_blank"><i style="color:#fff" class="fa fa-facebook fa-2x footer-facebook"></i></a>&nbsp;&nbsp;&nbsp; 
+                    <a href="" target="_blank"><i  style="color:#fff" class="fa fa-instagram fa-2x footer-instagram"></i></a>&nbsp;&nbsp;&nbsp; 
+                    <a href="" target="_blank"><i  style="color:#fff" class="fa fa-twitter fa-2x footer-twitter"></i></a>&nbsp;&nbsp;&nbsp; 
+                    <a href="" target="_blank"><i  style="color:#fff" class="fa fa-youtube-play fa-2x footer-youtube"></i></a>
+                </p>
             </div>
             <div class="col-sm-12 col-md-6">
-                Contact Form
-                <form style="form-group" method="post" id = "register_form">
-                    <input style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:40px" placeholder="Full Name" type="text" class="form-control" name="regisname" id="regisname">                        <input style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:40px" placeholder="Email" type="email" class="form-control" name="regisemail" id="regisremail">
-                    <input id="register" name="register" type="submit" style=";text-align:center;background-color:#fff;border:0px;color:#333;height:40px;padding:0 40px 0 40px;border-radius:3px;font-weight:bold;" value="SIGN UP"/>
-                    <a href="" style="margin-top:10px;float:right;color:#fff;font-size:14px;text-decoration:underline;text-align:right" data-toggle="modal" data-target="#signinModal" data-dismiss="modal">Sign In</a> 
+                <h5 style="font-size:17px;color:#888;margin-top:40px;margin-bottom:15px;">CONTACT FORM</h5>
+                <form style="margin-top:30px;margin-bottom:100px;">
+                    <input style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:40px" placeholder="Name" type="text" class="form-control">
+                    <input style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:40px" placeholder="Email" type="email" class="form-control">
+                    <input style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:40px" placeholder="Subject" type="text" class="form-control">
+                    <textarea name ="alamat" id = "alamat" rows="5" class="form-control" style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:100px;width:100%;" placeholder="Message"></textarea>
+                    <input id="register" type="submit" style=";text-align:center;background-color:#FF3D23;border:0px;color:#fff;height:40px;padding:0 40px 0 40px;border-radius:3px;font-weight:bold;" value="SEND"/>
+                    
                 </form>
             </div>
             
         </div>
     </div>
 </div>
-<!-- /Contact --> 
+<!-- /Contact -->
