251c92b9868f4ea37ff48137eb7cbaf4e06c2dc5
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-06 14:45:45 +0700

    perbaikan berhenti berlanganan

--- member/dashboard.php
+++ member/dashboard.php
@@ -260,7 +260,7 @@
 				  	<br/>
 					  	<div class="row">
 							<div class="col-sm-6">  
-								<a href="<?php echo $base_url_member; ?>/setup-progress" style=" text-decoration:none">
+								<a href="<?php echo $base_url_member; ?>/jobs-progress" style=" text-decoration:none">
 									<h5 style="color:white;">PROGRESS</h5>
 								  	<?php
 										$res = $col_history->find(array("status"=>"progress", "hal"=>"pasang"));
@@ -273,7 +273,7 @@
 								</a>  	
 							</div>  											
 							<div class="col-sm-6">  
-								<a href="<?php echo $base_url_member; ?>/setup-done" style=" text-decoration:none">
+								<a href="<?php echo $base_url_member; ?>/jobs-done" style=" text-decoration:none">
 									<h5 style="color:white;">DONE</h5>
 								  	<?php
 										$res = $col_history->find(array("status"=>"done", "hal"=>"pasang"));
