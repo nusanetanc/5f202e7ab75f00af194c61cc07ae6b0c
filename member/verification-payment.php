3c738dba1ee87c71681e1242f053b4df1c330ede
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-06 10:22:51 +0700

    perbaikan berhenti berlanganan

--- member/verification-payment.php
+++ member/verification-payment.php
@@ -226,7 +226,7 @@ if(isset($_POST['terminasi'])){
 		$month_tutup = bulan($bln_tutup);
 	$term=array("tanggal_berhenti"=>$termination_date, "alasan"=>$textalasanberhenti);
 	$update_user = $col_user->update(array("id_user"=>$id_cust, "level"=>"0"), array('$set'=>array("tanggal_akhir"=>$termination_date, "status"=>"Tidak Aktif"))); echo yudi;
-	$push_termiansi = $col_user->update(array("id_user"=>$id_cust, "level"=>"0"), array('$push'=>array("termination"=>$term)));
+	$push_termiansi = $col_user->update(array("id_user"=>$id_cust, "level"=>"0"), array('$push'=>array("termination"=>$term)));echo yudi0;
 				// mail for supevisior teknik
 				$subject0 = 'Request Ambil Perangkat';
 				$message0 = '
