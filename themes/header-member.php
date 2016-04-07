42b0d23814313f3cf53e94e0a6b2ad83d2f736ad
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-07 11:42:18 +0700

    perbaikan informasi di support

--- themes/header-member.php
+++ themes/header-member.php
@@ -53,9 +53,9 @@ if($level=="501"){
 <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
+        <span class="icon-bar" style="color:black;"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
