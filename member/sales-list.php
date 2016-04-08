252f5a41d59d39f777581537482316a5e0bf2872
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-08 14:43:29 +0700

    perbaikan dashboard admin

--- member/sales-list.php
+++ member/sales-list.php
@@ -10,6 +10,7 @@
 	  				    <form method="post">
 	  				    <div class="row">
 	  				    	<div class="col-sm-12">
+	  				    	<?php if($level=="5"){ ?>
 		  				    	<table class="table table-striped table-hover ">
 									 <thead>
 									    <tr>
@@ -34,6 +35,29 @@
 									  </tbody>
 									  <?php } ?>
 								</table>	  
+		  				    	<?php } elseif($level=="1"){ ?>
+		  				    	<table class="table table-striped table-hover ">
+									 <thead>
+									    <tr>
+									      <th width="20%">Id</th>
+									      <th width="40%">Nama</th>
+									      <th width="40%">Customer</th>
+									    </tr>
+									  </thead>
+									  <?php 
+
+									  		$rslt = $col_user->find(array("level"=>"501"));
+									  		foreach ($rslt as $row) {
+									  			$count_customer=$col_user->find(array("id_sales"=>$row['id_user']))->count();
+									   ?>
+									  <tbody>
+									  	<td><?php echo $row['id_user']; ?></td>
+									  	<td><?php echo $row['nama']; ?></td>
+									  	<td><?php echo $count_customer; ?></td>
+									  </tbody>
+									  <?php } ?>
+								</table>	  
+		  				    	<?php } ?>
 		  				    </div>
 		  				 </div>
 		  				 </form>
