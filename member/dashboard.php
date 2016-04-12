7ea52ade696b720c51d963842dd5a9904c7bfa08
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-12 11:36:59 +0700

    perbaikan halaman support

--- member/dashboard.php
+++ member/dashboard.php
@@ -13,7 +13,11 @@
 				</a>	
 			</div>	
 			<div class="col-sm-12 col-md-4 col-lg-4" >
+				<?php if($no_virtual=="" || $no_virtual==null){ ?>
 				<a href="<?php echo $base_url_member; ?>/billing"  style=" text-decoration:none">
+				<?php } elseif($no_virtual<>"" || $no_virtual<>null){  ?>
+				<a href="<?php echo $base_url_member; ?>"  style=" text-decoration:none">
+				<?php } ?>
 					<div class="well well-lg" style="background-color:#FF6D20;">
 					  <h4 style="color:white;"><b>BILLING</b></h4>
 					  <div class="text-center">
