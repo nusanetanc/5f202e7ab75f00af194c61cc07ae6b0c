8316f3822ce16a40716bec5d45ff47b88f462d3c
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-13 13:59:41 +0700

    perbaikan edit profile

--- member/editprofile.php
+++ member/editprofile.php
@@ -15,7 +15,7 @@
 							if($fileName<>"" || $fileName<>null){
 								$move = move_uploaded_file($lokasifile, "$dir".$fileName);
 								$update_user=$col_user->update(array("id_user"=>$id, "level"=>$level),array('$set'=>array("foto"=>$fileName)));
-  							} if(!(empty($_POST['editEmail'])) || $_POST['email']<>$email){
+  							} if(isset($_POST['editEmail']) || $_POST['email']<>$email){
   								$update_user=$col_user->update(array("id_user"=>$id, "level"=>$level),array('$set'=>array("email"=>$_POST['editEmail'])));	
   							}
   							} ?>
