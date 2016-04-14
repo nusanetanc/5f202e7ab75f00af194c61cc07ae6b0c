48962dde3aaf4702c8ea003bd50d9768a664e80a
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-14 14:42:45 +0700

    perbaikan edit profile

--- member/pengaduan.php
+++ member/pengaduan.php
@@ -1,9 +1,9 @@
 <?php
 if ($level=="0"){
-	if ($_GET['a']=="close"){
+	if (isset($_GET['a']){
 	$update = $col_ticket->update(
 								array("id_user"=>$id),
-								array('$set'=>array("status"=>"close"))); 
+								array('$set'=>array("status"=$_GET['a']))); 
 							}
  $res = $col_ticket->findOne(array("id_user"=>$id));
 if ($res['status']=="solved" || $res['status']=="open" || $_GET['c']<>''){
