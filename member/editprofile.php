e703f85411b1c7813026229dd38d96d2cb9189c9
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-14 11:41:19 +0700

    perbaikan edit profile

--- member/editprofile.php
+++ member/editprofile.php
@@ -42,33 +42,21 @@
 					</div>	
 					<div class="col-sm-9">	
 						<fieldset>
-						<?php if($_SESSION['emailedit']=="1"){ ?>
-						    <div class="form-group has-success">
-					    <?php } else { ?>
 						   	<div class="form-group">
-						    <?php } ?>
 						      <label for="editEmail" class="col-lg-2 control-label">Email</label>
 						      <div class="col-lg-10">
 						        <input type="email" class="form-control" id="editEmail" name="editEmail" value="<?php echo $email; ?>">
 						        <br/>
 						      </div>
 						    </div>	
-						<?php if($_SESSION['phoneedit']=="1"){ ?>
-						    <div class="form-group has-success">
-					    <?php } else { ?>
 						   	<div class="form-group">
-						    <?php } ?>
 						      <label for="editPhone" class="col-lg-2 control-label">Phone Number</label>
 						      <div class="col-lg-10">
 						        <input type="text" class="form-control" id="editPhone" name="editPhone" value="<?php echo $notelp; ?>">
 						        <br/>
 						      </div>
 						    </div>
-						<?php if($_SESSION['passwordedit']=="1"){ ?>
-						    <div class="form-group has-success">
-					    <?php } else { ?>
 						   	<div class="form-group">
-						    <?php } ?>
 						      <label class="col-lg-2 control-label">Ganti Password</label>
 						      <div class="col-lg-10">
 						        <input type="password" class="form-control" id="editPasswordlama" name="editPasswordlama" placeholder="Masukan Password Lama Anda">
@@ -84,12 +72,6 @@
 						      </div>
 						    </div>			    						    						    						    						    						    
 						  </fieldset>
-						  <?php
-						  		unset($_SESSION["fotoedit"]);
-						  		unset($_SESSION['emailedit']);
-						  		unset($_SESSION['phoneedit']);
-						  		unset($_SESSION['passwordedit']);
-						  ?>
 						</form>    		
 					</div>	
  				</div>
@@ -97,52 +79,12 @@
 		</div>
 	</div>	
 </section>
-<?php } else { ?>
-<?php	 
-if(isset($_SESSION['groovy_message'])){ 
-	?>
-	<script>
-		$(document).ready(function(){
-		$("#validate").show();
-	});
-	</script> <?php 
-					$groovy_message_status=$_SESSION['groovy_message_status'];
-					$groovy_message=$_SESSION['groovy_message'];
-				unset($_SESSION["groovy_message"]); 
-				unset($_SESSION["groovy_message_status"]);
-		} else { ?>
-	<script>
-	 $(document).ready(function(){
-		$("#validate").hide();
-	});	
-	</script> <?php } ?>
-<?php
-	if(isset($_POST['save'])){
-							$editPhone=$_POST['editPhone'];
-							$editPasswordlama=$_POST['editPasswordlama'];
-							$editPasswordbaru1=$_POST['editPasswordbaru1'];
-							$editPasswordbaru2=$_POST['editPasswordbaru2'];
-
-if ($editPhone==""){
-	$_SESSION['groovy_message_status']="danger";
-	$_SESSION['groovy_message']="Edit profile data failed";
-}							
-	 else if (($editPasswordlama<>"" || $editPasswordbaru1<>"" || $editPasswordbaru2<>"") && ($editPasswordlama=="" || $editPasswordbaru1=="" || $editPasswordbaru2=="" || $editPasswordbaru1<>$editPasswordbaru2 || $password<>$editPasswordlama)) { 
-				$_SESSION['groovy_message_status']="danger";
-				$_SESSION['groovy_message']="Edit profile data failed";
- } else {
- 	if ($editPasswordlama=="" || $editPasswordbaru1=="" || $editPasswordbaru2==""){
- 		$editPasswordbaru1=$password;
- 	}
-$update_user=$col_user->update(array("id_user"=>$id),array('$set'=>array("password"=>$editPasswordbaru1,"phone"=>$editPhone)));							
-if ($update_user){ 
-					$_SESSION['groovy_message_status']="success";
-					$_SESSION['groovy_message']="Edit profile data success";
- } } 
- 					?>
-						<script type="" language="JavaScript">
-						document.location='<?php echo $base_url_member; ?>/edit-profile'</script>
-					<?php } ?>
+<?php } else {
+if(isset($_POST['save0'])){  
+	if($_POST['editPasswordlama']<>"" && $_POST['editPasswordbaru1']<>"" && $_POST['editPasswordbaru2']<>"" && $_POST['editPasswordbaru1']==$_POST['editPasswordbaru2'] && $_POST['editPasswordlama']==$password && $_POST['editPasswordlama']<>$_POST['editPasswordbaru1']){
+			$update_user=$col_user->update(array("id_user"=>$id, "level"=>$level),array('$set'=>array("password"=>$_POST['editPasswordbaru1'])));
+			$_SESSION["passwordedit"]="1";
+		} } ?>
 <section>
 	<div class="col-sm-12" style="font-family:Arial;">
 		<div class="list-group">
@@ -155,42 +97,8 @@ if ($update_user){
   					<div class="col-sm-2">
   						<img class="profile-img-card profile-img-card-xlrg" src="./foto/<?php echo $foto ?>"/>						
 					</div>	
-					<div class="col-sm-10">	
-							<div name="validate" id="validate" class="alert alert-dismissible alert-<?php echo $groovy_message_status; ?>">
-							  	<button type="button" class="close" data-dismiss="alert">x</button>
-							  	<p><?php echo $groovy_message; ?></p>
-							</div>
 						<form enctype="multipart/form-data" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
 						  <fieldset>
-						    <div class="form-group">
-						      <label for="editNama" class="col-lg-2 control-label">Nama Lengkap</label>
-						      <div class="col-lg-10">
-						        <h4><?php echo $nama; ?></h4>
-						      	
-						      	<br/>
-						      </div>
-						    </div>
-						    <div class="form-group">
-						      <label for="editEmail" class="col-lg-2 control-label">Email</label>
-						      <div class="col-lg-10">
-						        <h4><?php echo $email; ?></h4>
-						        <br/>
-						      </div>
-						    </div>	
-						    <div class="form-group">
-						      <label class="col-lg-2 control-label">ID</label>
-						      <div class="col-lg-10">
-						        <h4><?php echo $id; ?></h4>
-						        <br/>
-						      </div>
-						    </div>
-						    <div class="form-group">
-						      <label for="editPhone" class="col-lg-2 control-label">Phone Number</label>
-						      <div class="col-lg-10">
-						        <input type="text" class="form-control" id="editPhone" name="editPhone" value="<?php echo $notelp; ?>">
-						        <br/>
-						      </div>
-						    </div>
 						    <div class="form-group">
 						      <label for="editPassword" class="col-lg-2 control-label">Password</label>
 						      <div class="col-lg-10">
@@ -202,7 +110,7 @@ if ($update_user){
 						        <br/>
 						        <div class="g-recaptcha" data-sitekey="6LfARxMTAAAAADdReVu9DmgfmTQBIlZrUOHOjR-8"></div>	
 						        <br/>
-						        <button class="btn btn-default" type="submit" name="save" id="save">Save Change</button>
+						        <button class="btn btn-default" type="submit" name="save0" id="save0">Save Change</button>
 						      </div>
 						    </div>			    						    						    						    						    						    
 						  </fieldset>	
