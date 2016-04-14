e08733ce5331634e0a311e3161de41d6be093cff
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-14 09:09:30 +0700

    perbaikan edit profile

--- member/editprofile.php
+++ member/editprofile.php
@@ -18,7 +18,7 @@
 								$_SESSION["fotoedit"]="Profile Photo Changed";
   							} if($_POST['editEmail']<>"" || $_POST['editEmail']<>$email){
   								$update_user=$col_user->update(array("id_user"=>$id, "level"=>$level),array('$set'=>array("email"=>$_POST['editEmail'])));
-  								//$_SESSION["emailedit"]="Email Changed";	
+  								$_SESSION["emailedit"]="Email Changed";	
   							} if($_POST['editPhone']<>"" || $_POST['editPhone']<>$notelp){
   								$update_user=$col_user->update(array("id_user"=>$id, "level"=>$level),array('$set'=>array("phone"=>$_POST['editPhone'])));	
   								$update_user=$col_history->update(array("id_cust"=>$id),array('$set'=>array("phone_customer"=>$_POST['editPhone'])));
