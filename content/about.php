e4911e9eaad088d8e4a13b2a3c3d224779929755
purwanto1337 <purwanto1337@gmail.com>
2016-04-19 02:39:48 +0000

    update about

--- content/about.php
+++ content/about.php
@@ -5,12 +5,12 @@
         <div class="col-sm-12 supportContentWrapper">
             <h3 style="margin:0;font-size:33px;font-weight:bold;">About Groovy</h3>
             <div style="background: linear-gradient(to right, #FF3D23 , #FF931E);height:2px;margin-top:30px;margin-bottom:5px;"></div>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Melindungi data pribadi dan menghargai privasi Anda</h6><br/>
+            <p style="line-height:1.5;font-size:15px;color:#444;">Melindungi data pribadi dan menghargai privasi Anda</p><br/>
             <p style="line-height:1.5;font-size:15px;color:#444;">HOOQ adalah pintu gerbang ke dunia hiburan tanpa batas. Dengan hanya satu aplikasi, Anda dapat melakukan streaming, download dan menikmati ribuan film dan serial TV. Semua dari ponsel, laptop atau tablet.</p>
             <br/>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Jika Anda ingin menonton Bollywood blockbuster terbaru, bersantai dengan rom-com Hollywood, menakut-nakuti diri dengan film horor Thai terbaru atau mengejar ketinggalan serial TV favorit Anda, HOOQ bisa membantu semuanya.</p>
+            <p style="line-height:1.5;font-size:15px;color:#444;">Jika Anda ingin menonton Bollywood blockbuster terbaru, bersantai dengan rom-com Hollywood, menakut-nakuti diri dengan film horor Thai terbaru atau mengejar ketinggalan serial TV favorit Anda, HOOQ bisa membantu semuanya.</p>
             <br/>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Informasi yang kami dapatkan</h6><br/>
+            <p style="line-height:1.5;font-size:15px;color:#444;">Informasi yang kami dapatkan</p><br/>
             <p style="line-height:1.5;font-size:15px;color:#444;">HOOQ berikan Anda akses tak terbatas ke lebih dari 10.000 film dan serial TV, dan akan berkembang setiap hari! Temukan petualangan tak berujung, drama dan non-stop komedi di ujung jari Anda. Bergabung dengan kami untuk memperluas dunia hiburan Anda.
             </p></div>
     </div>
