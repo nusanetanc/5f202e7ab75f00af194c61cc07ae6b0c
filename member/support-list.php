764d6cbd9341014ad3df3d2e15b311aeceebda4c
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-21 10:38:28 +0700

    perbaikan

--- member/support-list.php
+++ member/support-list.php
@@ -40,11 +40,63 @@
 									  <?php } } ?>
 								</table>
 								<?php } else if($level=="1"){ ?>
-									<table  class="table table-striped table-hover ">
-										<td>Data Bulan : </td>
-										<td>Total Pasang : </td>
-										<td>Total Bongkar : </td>
-									</table>
+									<form method="post">
+									<div class="row">
+										<div class="col-sm-12">
+											<div class="col-sm-4">
+												<select class="form-control" id="select">
+													<option>All</option>
+													<option>Pasang</option>
+													<option>Maintenance</option>
+												</select>
+										</div>
+										<div class="col-sm-4">
+												<select name="month" id="month" class="form-control" id="select">
+										<option value="" selected="true" disabled="true">Select Month</option>
+												<option value="01">Januari</option>
+												<option value="02">Februari</option>
+												<option value="03">Maret</option>
+												<option value="04">April</option>
+												<option value="05">Mei</option>
+												<option value="06">Juni</option>
+												<option value="07">Juli</option>
+												<option value="08">Agustus</option>
+												<option value="09">September</option>
+												<option value="10">Oktober</option>
+												<option value="11">November</option>
+												<option value="12">Desember</option>
+												</select>
+											</div>
+											<div class="col-sm-3">
+												<select name="year" id="year" class="form-control" id="select">
+													<option value="" selected="true" disabled="true">Select Years</option>
+													<option>2016</option>
+													<option>2017</option>
+													<option>2018</option>
+													<option>2019</option>
+													<option>2020</option>
+													<option>2021</option>
+													<option>2022</option>
+													<option>2022</option>
+													<option>2023</option>
+													<option>2024</option>
+													<option>2025</option>
+												</select>
+										</div>
+										<div class="col-sm-1">
+											<input name="search" id="search" type="submit" class="btn btn-primary" value="Search">
+										</div>
+								</div>
+							</div>
+							</form>
+							<table  class="table table-striped table-hover ">
+								<tbody>
+									<td>Data Bulan : </td>
+									<td>Total Pasang : </td>
+									<td>Total Bongkar : </td>
+							</tbody>
+							</table>
+									<br/>
 								<table class="table table-striped table-hover ">
 									 <thead>
 									    <tr>
