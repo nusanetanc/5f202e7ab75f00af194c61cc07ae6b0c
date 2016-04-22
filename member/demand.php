3367394be2259551d2d9e481fd92c9ac9b08afe6
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-22 14:01:54 +0700

    perbaikan halaman superadmin

--- member/demand.php
+++ member/demand.php
@@ -8,6 +8,35 @@
   				<div class="panel-body">
   					<br/>
 					<div class="col-sm-12">
+						<?php
+						if(isset($_GET['k'])){
+						?>
+						<table class="table table-striped table-hover ">
+							<thead>
+								<tr>
+									<th width="30%">Nama</th>
+									<th width="30%">Paket</th>
+									<th width="70%">Tempat</th>
+								</tr>
+							</thead>
+							<?php
+						?>
+							<tbody>
+								<?php
+									$k = str_replace('00',' ', $_GET['k']);
+									$res = $col_demand->find(array("kota"=>$k));
+									foreach($res as $row)
+									{
+								?>
+								<tr>
+									<td><?php $row['nama']; ?></td>
+									<td><?php $row['paket']; ?></td>
+									<td><?php $row['alamat']; ?></td>
+								</tr>
+								<?php } ?>
+							</tbody>
+					 </table>
+					 <?php } else { ?>
 						<table class="table table-striped table-hover ">
 						  <thead>
 						    <tr>
@@ -28,35 +57,36 @@
 						    <tr>
 						      <td>Jakarta Utara</td>
 									<td><?php echo $res_jakut; ?></td>
-									<td><a href="<?php echo $base_url_member; ?>/demand/jakut" class="btn btn-primary btn-xs">Lihat</a></td>
+									<td><a href="<?php echo $base_url_member; ?>/demand/Jakarta00Utara" class="btn btn-primary btn-xs">Lihat</a></td>
 						    </tr>
 								<tr>
 						      <td>Jakarta Selatan</td>
 									<td><?php echo $res_jaksel; ?></td>
-									<td><a href="<?php echo $base_url_member; ?>/demand/jaksel" class="btn btn-primary btn-xs">Lihat</a></td>
+									<td><a href="<?php echo $base_url_member; ?>/demand/Jakarta00Selatan" class="btn btn-primary btn-xs">Lihat</a></td>
 						    </tr>
 								<tr>
 						      <td>Jakarta Barat</td>
 									<td><?php echo $res_jakbar; ?></td>
-									<td><a href="<?php echo $base_url_member; ?>/demand/jakbar" class="btn btn-primary btn-xs">Lihat</a></td>
+									<td><a href="<?php echo $base_url_member; ?>/demand/Jakarta00Barat" class="btn btn-primary btn-xs">Lihat</a></td>
 						    </tr>
 								<tr>
 						      <td>Jakarta Timur</td>
 									<td><?php echo $res_jaktim; ?></td>
-									<td><a href="<?php echo $base_url_member; ?>/demand/jaktim" class="btn btn-primary btn-xs">Lihat</a></td>
+									<td><a href="<?php echo $base_url_member; ?>/demand/Jakarta00Timur" class="btn btn-primary btn-xs">Lihat</a></td>
 						    </tr>
 								<tr>
 						      <td>Jakarta Pusat</td>
 									<td><?php echo $res_jakpus; ?></td>
-									<td><a href="<?php echo $base_url_member; ?>/demand/jakpus" class="btn btn-primary btn-xs">Lihat</a></td>
+									<td><a href="<?php echo $base_url_member; ?>/demand/Jakarta00Pusat" class="btn btn-primary btn-xs">Lihat</a></td>
 						    </tr>
 								<tr>
-						      <td>Jakarta Bandung</td>
+						      <td>Bandung</td>
 									<td><?php echo $res_bdg; ?></td>
-									<td><a href="<?php echo $base_url_member; ?>/demand/bdg" class="btn btn-primary btn-xs">Lihat</a></td>
+									<td><a href="<?php echo $base_url_member; ?>/demand/Bandung" class="btn btn-primary btn-xs">Lihat</a></td>
 						    </tr>
 						   </tbody>
 						</table>
+						<?php } ?>
 					</div>
  				</div>
 			</div>
