9e3848949f47f4957f66fd6542f57d4579eef1ad
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-27 15:32:20 +0700

    penambahan add on

---
+++ member/add-on.php
@@ -0,0 +1,45 @@
+<section>
+	<div class="col-sm-9" style="font-family:Arial;">
+		<div class="list-group">
+			<div class="panel" style="border:0px;" >
+  				<div class="panel-heading" style="background-color:#FC9822">
+    				<h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">Add On Service</h3>
+  				</div>
+  				<div class="panel-body">
+  					<br/>
+  					<div class="col-sm-12">
+						<li class="list-group-item">
+						  	Add On :
+						  	<select class="form-control" id="upgrade_paket" name="upgrade_paket">
+						  			<option disabled="true" selected="true">Selected Service</option>
+					          <option>TV Chanel</option>
+										<option>Video on Demand</option>
+										<option>Cinema Box HD</option>
+					      </select>
+						</li>
+						<li class="list-group-item" style="border:2;">
+							Life time<br/>
+							Rp. 65.000,-<br/> <br/>
+							- Fox Sports 1<br/>
+							- Fox Sports 2<br/>
+							- Fox Sports 3<br/>
+						</li>
+						<br/>
+						<input type="submit" class="btn btn-warning" name="upgrade" id="upgrade" value="Konfirmasi">
+						<br/>
+					</div>
+ 				</div>
+			</div>
+			<div class="panel" style="border:0px;" >
+  				<div class="panel-heading" style="background-color:#FC9822">
+    				<h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">Histori Add On Service</h3>
+  				</div>
+  				<div class="panel-body">
+  					<br/>
+  					<div class="col-sm-12">
+					</div>
+ 				</div>
+			</div>
+		</div>
+	</div>
+</section>
