6822124c0709c5482566978390a7ec65c5992dc3
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-04-27 14:42:52 +0700

    penambahan add on

--- member/index.php
+++ member/index.php
@@ -1,6 +1,6 @@
 <?php
 	$base_url="http://groovy.id/beta";
-	$base_url_member="http://groovy.id/beta/member"; 
+	$base_url_member="http://groovy.id/beta/member";
 	$email_dens="yudi.nurhandi@nusa.net.id";
 /*	$base_url="http://localhost/groovy";
 	$base_url_member="http://localhost/groovy/member"; */
@@ -14,26 +14,28 @@ if (isset($_GET['hal'])){
 				if ($hal=="logout"){
 						include('../con/'.$hal.'.php');
 				} else {
-						include('../con/function.php');	
+						include('../con/function.php');
 						include('../themes/header-member.php');
 						include('../themes/linksrc.php');
 			$res = $col_menu->find(array("hakakses"=>$level, "file"=>$hal));
 			foreach($res as $row) {
 			      					$file=$row['file'];
-			} 
+			}
 			$res = $col_modul->find(array("hakakses"=>$level, "page"=>$hal));
 			foreach($res as $row) {
 			       					$page=$row['page'];
-			} 
+			}
 	if (empty($file) && empty($page)) {
-						include('../member/not-found.php');
+				header("location:".$base_url_member."/303");
 	} else {
 
 			if($hal<>"editprofile"){
 										include('../member/sidebar.php');
-						} 
-
+						}
+if(!file_exists('../member/'.$hal.'.php')){
+	header("location:".$base_url_member."/303");
+}
 					include('../member/'.$hal.'.php');
-			}	
+			}
 				}
 ?>
