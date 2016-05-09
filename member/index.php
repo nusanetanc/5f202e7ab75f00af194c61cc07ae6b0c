bde0c060edf08aa8d5b72dfe4fefcf260b7a8202
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-05-09 14:13:26 +0700

    perbaikan billing ppn

--- member/index.php
+++ member/index.php
@@ -2,6 +2,10 @@
 	$base_url="http://groovy.id/beta";
 	$base_url_member="http://groovy.id/beta/member";
 	$email_dens="yudi.nurhandi@nusa.net.id";
+	$biaya_instalasi=500000;
+	$biaya_cable=10000;
+	$biaya_router=40000;
+	$biaya_stb=45000;
 /*	$base_url="http://localhost/groovy";
 	$base_url_member="http://localhost/groovy/member"; */
 session_start();
