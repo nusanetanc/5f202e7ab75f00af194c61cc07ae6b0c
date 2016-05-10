823963e549ab06a06ce0c7f51792257b1eb84cce
purwanto1337 <purwanto1337@gmail.com>
2016-05-10 06:28:35 +0000

    penambahaan halaman term of use

--- content/term-of-use.php
+++ content/term-of-use.php
@@ -5,54 +5,30 @@
         <div class="col-sm-12 supportContentWrapper">
             <h3 style="margin:0;font-size:33px;font-weight:bold;">Groovy Privacy Policy</h3>
             <div style="background: linear-gradient(to right, #FF3D23 , #FF931E);height:2px;margin-top:30px;margin-bottom:5px;"></div>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Melindungi data pribadi dan menghargai privasi Anda</h6><br/>
-            <p style="line-height:1.5;font-size:15px;color:#444;">Groovy mengumpulkan informasi untuk dapat memahami kebutuhan Anda agar kami dapat memberikan yang terbaik. Hal ini membantu kami untuk meningkatkan pelayanan.<br/><br/>
-
-Kami berusaha untuk seterbuka dan setransparan mungkin dalam melayani Anda, karena kami paham bahwa privasi Anda sangatlah penting.</p>
-            <br/>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Cara kami mendapatkan informasi pribadi Anda</h6><br/>
-            <p style="line-height:1.5;font-size:15px;color:#444;">Kami mengumpulkan informasi pribadi Anda ketika Anda :<br/>
-<ul>
-    <li>mengunjungi situs web kami</li>
-    <li>mendaftar menjadi member groovy</li>
-    <li>menghubungi kami dengan pertanyaan atau permohonan untuk pertolongan</li></ul></p>
-            <br/>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Informasi yang kami dapatkan</h6><br/>
-            <p style="line-height:1.5;font-size:15px;color:#444;">Informasi yang kami dapatkan tergantung dari aktivitas dan posisi Anda atas produk groovy. Berikut adalah informasi yang kami dapatkan :<br/>
+            <!--<h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Melindungi data pribadi dan menghargai privasi Anda</h6><br/>-->
+            <p style="line-height:1.5;font-size:15px;color:#444;">
             <ul>
-                <li>Identitas Anda</li>
-                <li>Informasi akun Anda</li>
-                <li>Informasi atas penggunaan Anda terhadap produk kami</li>
+                <ol>Groovy adalah paket layanan internet, live TV, video on demand, social TV, catch-up TV dan radio streaming untuk personal di kompleks perumahan atau apartemen yang disediakan oleh PT Media Andalan Nusa bagi para pelangganya baik sendiri maupun bekerjasama dengan pihak ketiga lainya.</ol>
+                <ol>Rental Set Top Box adalah penyewaan Set Top Box (“STB”) untuk menikmati layanan live TV, video on demand, social TV, catch-up TV dan radio streaming.</ol>
+                <ol>Rental Router adalah penyewaan perangkat Router untuk menikmati layanan internet.</ol>
+                <ol>Perangkat yang disewakan kepada pelanggan adalah sepenuhnya milik PT Media Andalan Nusa.</ol>
+                <ol>Untuk keperluan instalasi dan perawatan layanan Groovy, pelanggan wajib memperkenankan teknisi untuk dapat dan memeriksa perangkat pada pelanggan.</ol>
+                <ol>Pelanggan wajib menjaga dan merawat perangkat yang disewakan. Apabila terjadi kerusakan/kehilangan perangkat, maka segala biaya penggantian/perbaikan akan dibebankan kepada pelanggan.</ol>
+                <ol>Layanan Groovy dapat dinikmati oleh pelanggan yang telah memenuhi kewajibannya selama 24jam/7hari, kecuali apabila terjadi peristiwa Force Majeure dan/atau peristiwa lain di luar kendali.</ol>
+                <ol>Pelanggan tidak diperbolehkan untuk menghubungkan layanan Groovy dengan perangkat lain apapun yang bertujuan untuk tindakan atau kegiatan komersialisasi dalam bentuk apapun.</ol>
+                <ol>Pelanggan tidak diperbolehkan melakukan perubahan apapun terhadap konfigurasi layanan Groovy ataupun perangkat yang disewakan.</ol>
+                <ol>Apabila terjadi gangguan pada layanan Groovy, pelanggan dapat membuat pengaduan di halaman member.</ol>
+                <ol>Pelanggan akan dikenakan biaya instalasi sebesar Rp. 500.000,- pada awal pemasangan.</ol>
+                <ol>Proses instalasi akan dilakukan dengan diberikan kabel sampai dengan 25 meter, bila lebih dari itu pelanggan akan dibebankan biaya kabel tambahan sebesar Rp. 10.000,- / meter</ol>
+                <ol>Pelanggan akan dibebankan biaya sewa perangkat Router Wifi sebesar Rp. 40.000,- / bulan</ol>
+                <ol>Pelanggan akan dibebankan biaya sewa Set Top Box (STB) Rp. 45.000,- / bulan untuk paket GH500, GH800 dan GH1700</ol>
+                <ol>Sat akun (account) hanya dapat menggunakan satu Router Wifi dan satu STB</ol>
+                <ol>Layanan Groovy akan menggunakan IP local dynamic</ol>
+                <ol>Layanan Groovy hanya untuk pemakaian rumah dan tidak untuk dijual kembali.</ol>
             </ul>
             </p>
-            <br/>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Kami menggunakan Informasi Anda</h6><br/>
-            <p style="line-height:1.5;font-size:15px;color:#444;">bagaimana kami menggunakan informasi Anda, diantaranya adalah :<br/>
-            <ul>
-                <li>untuk menyediakan layanan administrasi Anda</li>
-                <li>untuk penelitian pasar dan peningkatan layanan</li>
-                <li>untuk persyaratan hukum dan peraturan</li>
-            </ul>
-            </p>
-            <br/>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Membagikan informasi Anda</h6><br/>
-            <p style="line-height:1.5;font-size:15px;color:#444;">Kami dapat membagikan informasi Anda dengan :<br/>
-            <ul>
-                <li>rekan usaha dan vendor yang bekerja sama dengan kami untuk memberikan layanan yang Anda sudah berlangganan atasnya.</li>
-                <li>regulator industri dan organisasi pemerintahan lainya, sebagaimana dipersyaratkan oleh hukum dan peraturan setempat</li>
-                <li>inistitusi keuangan untuk tujuan seperti memfasilitasi pembayaran GIRO</li>
-                <li>institusi penelitian untuk tujuan analisa pasar</li>
-            </ul>
-            </p>
-            <br/>
-            <h6 style="font-size:21px;font-weight:600;line-height:24px;margin:25px 0 0 0;color:#444">Melindungi informasi Anda</h6><br/>
-            <p style="line-height:1.5;font-size:15px;color:#444;">Kami selalu menerapkan sistem keamanan yang ketat untuk melindungi informasi Anda, upaya yang kami terapkan diantaranya :<br/>
-            <ul>
-                <li>membatasi akses terhadap informasi di sistem kami dengan sistem dari rekan bisnis dan vendor kami</li>
-                <li>menjaga untuk mencegah pelanggaran keamanan di jaringan dan sistem database kami</li>
-                <li>memperketat proses verifikasi untuk mencegah akses yang tidak diiginkan</li>
-            </ul>
-            </p>
+                
+                
             <br/>
             <h6 style="font-size:16px;font-weight:500;line-height:24px;margin:25px 0 0 0;color:#444">Butuh pertanyaan lebih detail, silahkan hubungi kami <a href="<?php echo $base_url; ?>/contact-us">disini</a></h6><br/>
             <p style="line-height:1.5;font-size:15px;color:#444;">Kami dapat mengubah kebijakan ini dari waktu ke waktu. Kami akan selalu berkomitmen untuk menjaga informasi Anda dan terbuka mengenai praktek perlindungan data kami.</p>
