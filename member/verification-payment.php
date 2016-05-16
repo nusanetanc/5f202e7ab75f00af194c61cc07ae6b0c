083d5e2e56bd577a643ee33358e2f446ccf14b4f
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-05-16 10:59:43 +0700

    perbaikan payment

--- member/verification-payment.php
+++ member/verification-payment.php
@@ -127,13 +127,22 @@ $kol_stb = array(
     array("label"=>rupiah($biaya_stb), "length"=>30, "align"=>"C"),
     array("label"=>rupiah($proraide_stb), "length"=>30, "align"=>"C"),
     array("label"=>rupiah($biaya_stb+$proraide_stb), "length"=>30, "align"=>"C")
-  );
+  ); if($status_cust=="registrasi"){
   $kol_instalasi = array(
-      array("label"=>"Biaya Instalasi", "length"=>80, "align"=>"C"),
+      array("label"=>"Instalasi", "length"=>80, "align"=>"C"),
       array("label"=>rupiah($biaya_instalasi), "length"=>30, "align"=>"C"),
       array("label"=>rupiah($proraide_instalasi), "length"=>30, "align"=>"C"),
       array("label"=>rupiah($biaya_instalasi+$proraide_instalasi), "length"=>30, "align"=>"C")
-    );
+    ); }
+  $res = $col_addon->find(array("id_user"=>"$id_cust"));
+          foreach($res as $row) {
+    $row['_id'] = array(
+        array("label"=>$row['layanan'], "length"=>80, "align"=>"C"),
+        array("label"=>rupiah($row['harga']), "length"=>30, "align"=>"C"),
+        array("label"=>rupiah($row['proraide']), "length"=>30, "align"=>"C"),
+        array("label"=>rupiah($row['harga']+$row['proraide']), "length"=>30, "align"=>"C")
+      );
+          }
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->Image('../img/groovy-logo-orange.png','140','15','60');
@@ -173,6 +182,12 @@ $pdf->Ln();
 foreach ($kol_router as $kolom_router) {
   $pdf->Cell($kolom_router['length'], 8, $kolom_router['label'], 1, '0', $kolom_router['align'], true);
 }
+$res = $col_addon->find(array("id_user"=>"$id_cust"));
+        foreach($res as $row) {
+$pdf->Ln();
+foreach ($row['_id'] as $kolom_addon) {
+  $pdf->Cell($kolom_addon['length'], 8, $kolom_addon['label'], 1, '0', $kolom_addon['align'], true);
+} }
 $pdf->Ln();
 foreach ($kol_stb as $kolom_stb) {
   $pdf->Cell($kolom_stb['length'], 8, $kolom_stb['label'], 1, '0', $kolom_stb['align'], true);
