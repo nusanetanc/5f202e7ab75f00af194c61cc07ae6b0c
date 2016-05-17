880fa74719d60744b54c4dbae5b490a9c2970bbd
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-05-17 09:21:03 +0700

    fixing payment

--- member/verification-payment.php
+++ member/verification-payment.php
@@ -175,7 +175,7 @@ $pdf->Ln();
 $pdf->SetFont('Arial','B','7');
 $pdf->SetFillColor(143, 173, 204);
 $pdf->SetTextColor(0);
-$pdf->SetDrawColor(0143, 173, 204);
+$pdf->SetDrawColor(143, 173, 204);
 foreach ($kol_paket as $kolom_paket) {
   $pdf->Cell($kolom_paket['length'], 8, $kolom_paket['label'], 1, '0', $kolom_paket['align'], true);
 }
