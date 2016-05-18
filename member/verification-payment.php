7be2dd06ec857c3963d7e18272473e74df2d68a9
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-05-18 14:15:39 +0700

    perbaikan payment

--- member/verification-payment.php
+++ member/verification-payment.php
@@ -742,9 +742,9 @@ if ($update_user && $emailbongkar && $emailnotice && $sent){
                                 </div>
                           </div>
                           <div class="modal-footer">
-                            <p>Total Tagihan : <?php echo $byr['total_tagihan']; ?></p>
-                            <p>PPN 10% : <?php echo $byr['ppn']; ?></p>
-                            <p>Total Pembayaran : <?php echo $byr['total_pembayaran']; ?></p>
+                            <p>Total Tagihan : <b><?php echo $byr['total_tagihan']; ?></b></p>
+                            <p>PPN 10% : <b><?php echo $byr['ppn']; ?></b></p>
+                            <p>Total Pembayaran : <b><?php echo $byr['total_pembayaran']; ?></b></p>
                           </div>
                         </div>
                       </div>
