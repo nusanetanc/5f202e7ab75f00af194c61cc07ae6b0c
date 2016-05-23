12c1e0dc5fdb5989610ab425db792f8770548814
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-05-23 10:47:04 +0700

    perbaikan payment

--- themes/header.php
+++ themes/header.php
@@ -365,6 +365,9 @@
                                 <option><?php echo $row['nama']; ?></option>
                                 <?php } ?>
                             </select>
+                            <select style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:40px" class="form-control" name="regisaddon" id="regispackage">
+                                <option disabled="true" selected="true">-- Add On --</option>
+                            </select>
                             <select style="background-color:rgba(255, 255, 255, 0.7);margin-bottom:9px;height:40px" class="form-control" name="regislocation" id="regislocation" disabled="true">
                                 <option disabled="true" selected="true">-- Location --</option>
                                 <?php
