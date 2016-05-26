07ec1a48f0cb6b133161025b49a925fe6770e504
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-05-26 14:54:03 +0700

    perbaikan payment

--- member/customer-profile.php
+++ member/customer-profile.php
@@ -220,9 +220,9 @@
 						    <div class="form-group">
 						      <label class="col-lg-3 control-label">KTP :</label>
 						      <div class="col-lg-9">
-							      <a href="#struk" class="btn btn-primary btn-xs">Open</a>
+							      <a href="" class="btn btn-primary btn-xs">Open</a>
 								  	<a href="" class="JesterBox">
-									  <div id="struk"><img src="<?php echo $base_url_member; ?>/ktp/<?php echo $ktp_cust; ?>"></div>
+									  <div id=""><img src="<?php echo $base_url_member; ?>/ktp/<?php echo $ktp_cust; ?>"></div>
 									</a>
 							  </div>
 							  </label>
@@ -254,7 +254,7 @@
 						   </div>
 							  </fieldset>
 							<?php
-							if($level=="5" && $status_cust=="permintaan registrasi" && $regis_cust=="sales"){ ?>
+							if($level=="5" && $status_cust=="permintaan registrasi" &1 HD Movie, Winning Time,& $regis_cust=="sales"){ ?>
 							 		<td><input type="submit" name="konfirmregis" id="konfirmregis" class="btn btn-primary btn-sm" value="Konfirmasi Registrasi"></td>
 							<?php } elseif($regis_cust=="konfirmasi registrasi") { ?>
 							<div class="form-group">
