dc8facb988db14cb7e7679442fd7a019904924ca
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-05-30 09:56:36 +0700

    perbaikan payment

--- member/invoice-customer.php
+++ member/invoice-customer.php
@@ -1,43 +1,91 @@
 <section>
 	<div class="col-sm-9" style="font-family:Arial;">
 		<div class="list-group">
-			<div class="panel" style="border:0px;">
-  				<div class="panel-heading" style="background-color:#FF6D20">
-    				<h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">Kirim Invoice Customer</h3>
-  				</div>
-	  					<br/>
-	  				    <div class="panel-body">
-	  				    <form enctype="multipart/form-data" action="<?php $_SERVER['PHP_SELF'] ?>"  method="post">
-	  				    <div class="row">
-	  				    	<div class="col-sm-12">
-		  				    	<form class="form-horizontal">
-								  <fieldset>
-									<div class="form-group">
-									  <label class="control-label" for="inputPaket">Nama Paket</label>
-									  <input type="text" class="form-control" id="inputPaket" name="inputPaket" placeholder="Masukan Nama Paket">
-									</div>
-									<div class="form-group">
-									  <label class="control-label" for="inputHarga">Harga</label>
-									  <input type="text" class="form-control" id="inputHarga" name="inputHarga" placeholder="Masukan Harga Paket">
-									</div>
-									<div class="form-group">
-									  <label class="control-label" for="inputHargahari>Harga/Hari</label>
-									  <input type="text" class="form-control" id="inputHargahari" name="inputHargahari" placeholder="Masukan Harga Paket Perhari">
-									</div>
-									<div class="form-group">
-									  <label class="control-label" for="inputDeskripsi">Deskripsi</label>
-									  <textarea name="inputDeskripsi" id="inputDeskripsi" class="form-control" rows="3" id="textArea" placeholder="Masukan Deskripsi Paket"></textarea>
-									</div>
-									<div class="form-group">
-									<input type="submit" id="submit" name="submit" class="btn btn-block" style="background-color:#FF6D20; color:white; font-weight:600;" value="Submit">
-									</div>
-								   </fieldset>
-								</form>
-		  				    </div>
-		  				 </div>
-		  				 </form>
-		  				 </div>
-		  	</div>
+			<div class="panel" style="border:0px;" >
+				<div class="panel-body" style="background-color:#1B5E12;">
+					<h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">CUSTOMER</h3>
+				</div>
+				<div class="panel-body">
+					<br/>
+					<div class="col-sm-12">
+						<form class="form-horizontal">
+							<fieldset>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Nama : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $nama_cust; ?></h4>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Email : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $email_cust; ?></h4>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Custommer ID : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $id_cust ?></h4>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Phone Number : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $phone_cust; ?></h4>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Paket Aktif/Harga : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $package_cust.'/'.rupiah($harga_paket); ?></h4>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Tambahan Layanan/Harga : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $package_cust.'/'.rupiah($harga_paket); ?></h4>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Lokasi : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $tempat_cust.', '.$ket_cust.', '.$kota_cust; ?></h4>
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="col-lg-3 control-label">Tanggal Registrasi : </label>
+								<div class="col-lg-9">
+								<h4><?php echo $tgl_registrasi.' '.$month_registrasi.' '.$thn_registrasi; ?></h4>
+								</div>
+							</div>
+						</div>
+							</fieldset>
+						</form>
+					</div>
+				</div>
+				<div class="panel" style="border:0px;" >
+					<div class="panel-body" style="background-color:#1B5E12;">
+						<h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">CUSTOMER</h3>
+					</div>
+					<div class="panel-body">
+						<br/>
+						<div class="col-sm-12">
+							<form class="form-horizontal">
+								<fieldset>
+									<div class="checkbox">
+											<label>
+												<input type="checkbox"> STB <br/>
+												<input type="checkbox"> Router <br/>
+												<input type="checkbox"> kabel <br/>
+												<input type="number" class="form-control" id="pjkbl" name="pjkbl" placeholder="Panjang Kabel (Meter)"><br/>
+												<input type="checkbox"> Instalasi
+											</label>
+										</div>
+								</fieldset>
+							</div>
+							</form>
+						</div>
+					</div>
 		</div>
 	</div>
 </section>
