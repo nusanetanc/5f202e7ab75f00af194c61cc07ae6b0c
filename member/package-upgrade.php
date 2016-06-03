57700549a1010ea0b34890f72ec459b095ed9d9d
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-06-03 13:42:32 +0700

    perbaikan add on

--- member/package-upgrade.php
+++ member/package-upgrade.php
@@ -36,8 +36,10 @@ foreach($res as $row)
 											Paket : '.$upgrade_paket.'<br/>';
 	if(!empty($_POST['addon'])){
 			foreach($_POST['addon'] as $selectaddon=>$valaddon){
-						'
-											Layanan Tambahan : '.$selectaddon.'<br/>'; } }
+						${$selectaddon} = $valaddon;
+				} }
+							$message1 = '
+											Layanan Tambahan '.$valaddon.' : '.$selectaddon.' : '.${$selectaddon}.'<br/>';
 							$message2 = '
 											Tanggal permintaan : '.$tgl0.' '.$month0.' '.$thn0.'</p>
 											<br/>';
