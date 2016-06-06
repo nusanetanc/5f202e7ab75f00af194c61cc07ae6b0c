7e08f466e6f5b3f418705d70de3cc19b3fed9ad4
purwanto1337 <purwanto1337@gmail.com>
2016-06-06 04:17:32 +0000

    perubahan konten about

--- content/about.php
+++ content/about.php
@@ -10,29 +10,23 @@
             <h3 style="font-size:23px">Paket internet dengan fitur menarik</h3><br/>
             <div class="row">
                 <div class="col-sm-6">
-                    <img style="float:left;width:100px;margin-right:20px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
                     <h4>High Speed Internet Access</h4>
                     <p style="line-height:1.5;font-size:14px;color:#444;">Layanan internet berkecepatan tinggi yang menghadirkan koneksi fiber optic langsung ke komplek perumahan / apartemen Anda.</p>
                     <br/>
-                    <img style="float:left;width:100px;margin-right:20px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
                     <h4>Live TV</h4>
                     <p style="line-height:1.5;font-size:14px;color:#444;">Layanan yang menghadirkan siaran langsung televisi nasional dan internasional dengan menggunakan alat STB terbaik.</p>
                     <br/>
-                    <img style="float:left;width:100px;margin-right:20px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
                     <h4>Video on Demand</h4>
                     <p style="line-height:1.5;font-size:14px;color:#444;">Layanan tambahan yang menghadirkan berbagai macam video on demand berkualitas.</p>
                     <br/>
                 </div>
                 <div class="col-sm-6">
-                    <img style="float:left;width:100px;margin-right:20px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
                     <h4>Social TV</h4>
                     <p style="line-height:1.5;font-size:14px;color:#444;">Layanan streaming yang menghadirkan berbagai macam video dan channel-channel terbaik yang sudah dikategorikan berdasarkan keinginan Anda.</p>
                     <br/>
-                    <img style="float:left;width:100px;margin-right:20px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
                     <h4>Catch-up TV Channel</h4>
                     <p style="line-height:1.5;font-size:14px;color:#444;">Menghadirkan siaran ulang televisi lokal & internasional sampai dengan 3 hari kebelakang.</p>
                     <br/>
-                    <img style="float:left;width:100px;margin-right:20px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
                     <h4>Radio Streaming</h4>
                     <p style="line-height:1.5;font-size:14px;color:#444;">Siarkan langsung radio lokal dan internasional dengan kualitas suara terbaik.</p>
                     <br/>
