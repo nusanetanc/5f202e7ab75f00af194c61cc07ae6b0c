ea72a066ada102835b4a58f461810d462fe1c3ab
purwanto1337 <purwanto1337@gmail.com>
2016-06-06 03:47:36 +0000

    perubahan about

--- content/about.php
+++ content/about.php
@@ -6,8 +6,21 @@
         <div class="col-sm-12 supportContentWrapper">
             <h3 style="margin:0;font-size:33px;font-weight:bold;">About Groovy</h3>
             <div style="background: linear-gradient(to right, #FF3D23 , #FF931E);height:2px;margin-top:30px;margin-bottom:5px;"></div>
-            <br/><p style="line-height:1.5;font-size:18px;color:#444;">Groovy adalah paket layanan Internet berkecepatan tinggi dengan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio streaming.
-</p></div>
+            <br/><p style="line-height:1.5;font-size:18px;color:#444;">Groovy adalah paket layanan Internet berkecepatan tinggi dengan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio streaming.<br/></p>
+            <h4>Paket internet dengan penawaran terbaik</h4>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    Internet Fiber Optic<br/>
+                    <img src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    Internet Fiber Optic<br/>
+                    
+                </div>
+                <div class="col-sm-6">
+                    b
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </div>
