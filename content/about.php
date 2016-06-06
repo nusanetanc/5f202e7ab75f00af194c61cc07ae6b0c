36bbc82f786fcd573dbfd34682084a48bdfc07a4
purwanto1337 <purwanto1337@gmail.com>
2016-06-06 03:54:09 +0000

    perubahan about

--- content/about.php
+++ content/about.php
@@ -4,20 +4,38 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12 supportContentWrapper">
-            <h3 style="margin:0;font-size:33px;font-weight:bold;">About Groovy</h3>
+            <h2 style="margin:0;font-size:33px;font-weight:bold;">About Groovy</h2>
             <div style="background: linear-gradient(to right, #FF3D23 , #FF931E);height:2px;margin-top:30px;margin-bottom:5px;"></div>
-            <br/><p style="line-height:1.5;font-size:18px;color:#444;">Groovy adalah paket layanan Internet berkecepatan tinggi dengan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio streaming.<br/></p>
-            <h4>Paket internet dengan penawaran terbaik</h4>
+            <br/><p style="line-height:1.5;font-size:18px;color:#444;">Groovy adalah paket layanan Internet berkecepatan tinggi dengan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio streaming.<br/><br/></p>
+            <h3 style="font-size:23px">Paket internet dengan penawaran terbaik</h3>
             <div class="row">
                 <div class="col-sm-6">
-                    <img src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
-                    Internet Fiber Optic<br/>
-                    <img src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
-                    Internet Fiber Optic<br/>
-                    
+                    <img style="float:left;width:100px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    <h4>High Speed Internet Access</h4>
+                    <p style="line-height:1.5;font-size:18px;color:#444;">ngan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio s</p>
+                    <br/>
+                    <img style="float:left;width:100px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    <h4>High Speed Internet Access</h4>
+                    <p style="line-height:1.5;font-size:18px;color:#444;">ngan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio s</p>
+                    <br/>
+                    <img style="float:left;width:100px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    <h4>High Speed Internet Access</h4>
+                    <p style="line-height:1.5;font-size:18px;color:#444;">ngan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio s</p>
+                    <br/>
                 </div>
                 <div class="col-sm-6">
-                    b
+                    <img style="float:left;width:100px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    <h4>High Speed Internet Access</h4>
+                    <p style="line-height:1.5;font-size:18px;color:#444;">ngan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio s</p>
+                    <br/>
+                    <img style="float:left;width:100px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    <h4>High Speed Internet Access</h4>
+                    <p style="line-height:1.5;font-size:18px;color:#444;">ngan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio s</p>
+                    <br/>
+                    <img style="float:left;width:100px;" src="http://jkt.nusa.net.id/wp-content/uploads/2015/11/speed2.png"/>
+                    <h4>High Speed Internet Access</h4>
+                    <p style="line-height:1.5;font-size:18px;color:#444;">ngan menggunakan kabel fiber optic yang langsung terhubung ke rumah / apartemen Anda. Layanan internet terbaik untuk komplek perumahan dan apartemen dengan mengkombinasikan layanan hiburan terlengkap mulai dari live TV, video on demand, social TV, catch-up TV dan radio s</p>
+                    <br/>
                 </div>
             </div>
         </div>
