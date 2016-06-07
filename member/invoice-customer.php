9366662302a36a63d923166e16017205166d2d51
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-06-07 14:08:34 +0700

    perbaikan invoice

--- member/invoice-customer.php
+++ member/invoice-customer.php
@@ -64,6 +64,11 @@ $payment_kabel = array (
 			$update_user=$col_user->update(array("id_user"=>$id_cust, "level"=>"0"),array('$set'=>array("no_virtual"=>$kode_perusahaan.$id_cust, "payment_data"=>array($payment_paket, $payment_router, $payment_stb, $payment_kabel, $payment_instal))));
 			foreach($_POST['addon'] as $addon_select)
 			{
+				$res = $col_service->find(array("nama"=>$addon_select));
+				foreach($res as $row)
+									{
+										$addon_harga=$row['harga']
+									}
 				$update_user1= $col_user->update(array("id_user"=>$id_cust, "level"=>"0"),array('$push'=>array("payment_data"=>array("layanan"=>$addon_select, "harga"=>$addon_harga, "prorate"=>$addon_prorate, "total"=>$addon_harga-$addon_prorate))));
 			}
 // mail for customer to addon
