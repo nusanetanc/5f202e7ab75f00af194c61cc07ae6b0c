229e8e0c66c1ac3dec864e6319bd45111f625b16
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-06-15 09:59:28 +0700

    update dashboard billing

--- member/detail-maintenance.php
+++ member/detail-maintenance.php
@@ -119,79 +119,6 @@ if ($update_user && $update_user1 && $insert_activty && $kirim_email && $kirim_e
 	<script type="" language="JavaScript">
 	document.location='<?php echo $base_url_member; ?>/detail-maintenance/<?php echo $id_cust; ?>'</script>
 <?php } } ?>
-
-<?php if (isset($_POST['btnupdate'])){
-	$input_paket=$_POST['paket_update'];
-	$histori=array("hal"=>"update", "tanggal_update"=>$date, "paket_lama"=>$package_cust, "paket_baru"=>$input_paket);
-	$update_user = $col_user->update(array("id_user"=>$id_cust, "level"=>"0"),array('$push'=>array("histori"=>$histori)));
-	$insert_activty = $col_history->insert(array("hal"=>"update","tanggal_update"=>$date, "paket_lama"=>$package_cust, "paket_baru"=>$input_paket, "id_cust"=>$id_cust, "nama_cust"=>$nama_cust, "tempat_customer"=>$tempat_cust, "alamat_customer"=>$alamat_cust, "kota_customer"=>$kota_cust ,"keterangan_customer"=>$ket_cust, "phone_customer"=>$phone_cust,"no_box"=>$no_box));
-	$update_user1 = $col_user->update(array("id_user"=>$id_cust, "level"=>"0"),array('$set'=>array("paket"=>$input_paket, "move_paket"=>"", "move_harga"=>"", "move_request"=>"")));
-$res = $col_package->find(array("nama"=>$package_cust));
-	foreach($res as $row)
-	{
-		$deskripsi_paket0=$row['deskripsi'];
-	}
-$res = $col_package->find(array("nama"=>$input_paket));
-	foreach($res as $row)
-	{
-		$deskripsi_paket1=$row['deskripsi'];
-	}
-	// mail for customer
-$subject1 = 'Pemberitahuan Pindah Paket';
-$message1 = '
-<html>
-	<body style="background-color:#ddd;padding:50px 0 50px 0;font-family:arial;font-size:15px;">
-			<div style="margin:0 auto;max-width:500px;background-color:#eee;-moz-border-radius: 0px;-webkit-border-radius: 5px 5px 5px 5px;border-radius: 5px 5px 5px 5px;">
-					<div style="background: linear-gradient(to right, #FF3D23 , #fc742f);-moz-border-radius: 0px;-webkit-border-radius: 5px 5px 0px 0px;border-radius: 5px 5px 0px 0px;padding:5px 0 2px 0;text-align:center;">
-							<a href="http://www.groovy.id"><img src="http://groovy.id/beta/img/groovy-logo-white.png" height="50px;"/></a>
-					</div>
-					<div style="padding:20px;color:#333;">
-							<p style="font-size:20px;font-weight:bold;line-height:1px">Pemberitahuan Pindah Paket</p>
-							<p>Kami sudah mengganti paket anda dari paket : '.$package_cust.' ('.$deskripsi_paket0.'), ke paket : '.$input_paket.' ('.$deskripsi_paket1.').</p>
-					</div>
-					</div>
-			</div>
-	</body>
-	</html>
-';
-$headers1  = 'MIME-Version: 1.0' . "\r\n";
-$headers1 .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers1 .= 'From: cs@groovy.id' . "\r\n";
-$headers1 .= 'Cc: cs@groovy.id' . "\r\n";
-$emailcust_pindah=mail($email_cust, $subject1, $message1, $headers1);
-	// mail for billing
-	$subject = 'Update Paket';
-
-	$message = '
-	<html>
-	<body>
-	  <p>Paket Telah Di Update : </p>
-	  <br/>
-	  <p>Customer : '.$id_cust.' / '.$nama_cust.' / '.$phone_cust.' / '.$email_cust.'</p>
-	  <p>Paket Lama : '.$package_cust.'</p>
-	  <p>Paket Baru : '.$input_paket.'</p>
-	  <p>Tempat : '.$tempat_cust.' '.$ket_cust.' '.$alamat_cust.' '.$kota_cust.'</p>
-	  <p>No STB : '.$no_box.'</p>
-	  <p>Tanggal Update : '.$date_days.'-'.$date_month.'-'.$date_years.'</p>
-	  <br/>
-	</body>
-	</html>
-	';
-
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-	$headers .= 'From: support@groovy.id' . "\r\n";
-	$headers .= 'Cc: cs@groovy.id' . "\r\n";
-	$res_bill = $col_user->find(array("level"=>"2"));
-							foreach ($res_bill as $row_bill) {
-								$kirim_email=mail($row_bill, $subject, $message, $headers);
-							}
-
-if ($update_user && $update_user1 && $insert_activty && $kirim_email){ ?>
-	<script type="" language="JavaScript">
-	document.location='<?php echo $base_url_member; ?>/detail-maintenance/<?php echo $id_cust; ?>'</script>
-<?php } } ?>
 <style>
     .datepicker{z-index:1151;}
 </style>
@@ -355,33 +282,6 @@ if ($update_user && $update_user1 && $insert_activty && $kirim_email){ ?>
 							</table>
 						</div>
 					</div>
-			</div>
-			<div class="panel" style="border:0px;">
-  				<div class="panel-heading" style="background-color:#F1453C">
-    				<h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">UPDATE</h3>
-  				</div>
-  				<div class="panel-body">
-  					<br/>
-					<div class="form-group">
-				      <label for="paket_update" class="col-lg-3 control-label">Paket</label>
-				      <div class="col-lg-9">
-				        <select class="form-control" id="paket_update" name="paket_update">
-				          <option disabled="true" selected="true" value="">Selected Package</option>
-				          <?php
-					         $res = $col_package->find();
-							foreach ($res as $row) { if($row['nama']<>$package_cust){ ?>
-				          <option value="<?php echo $row['nama']; ?>"><?php echo $row['nama'].' - '.$row['deskripsi']; ?></option>
-				          <?php } } ?>
-				        </select>
-				        <br/>
-				      </div>
-				    </div>
-				    <div class="col-lg-9">
-				        <div class="g-recaptcha" data-sitekey="6LfARxMTAAAAADdReVu9DmgfmTQBIlZrUOHOjR-8"></div>
-				        <br/>
-				      	<button class="btn btn-primary btn-sm" type="submit" name="btnupdate" id="btnupdate"><b>UPDATE</b></button>
-				    </div>
- 				</div>
 			</div>
 			<div class="panel" style="border:0px;">
 					<div class="panel-heading" style="background-color:#F1453C">
