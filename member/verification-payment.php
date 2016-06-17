e5f5b13ae16746c017322dc0482732c9ff744a08
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-06-17 09:35:11 +0700

    perbaikan dan update

--- member/verification-payment.php
+++ member/verification-payment.php
@@ -890,6 +890,19 @@ if($emailcust && $update_user && $push_histori){ ?>
 					  	</div>
 					</div>
 				</div>
+        <div class="col-sm-12">
+        <div class="list-group">
+          <div class="panel" style="border:0px;">
+              <div class="panel-heading" style="background-color:#1B5E12">
+                <h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">LOG</h3>
+              </div>
+                  <br/>
+                    <div class="panel-body">
+                      
+                   </div>
+                  </div>
+              </div>
+            </div>
 			</div>
 		</div>
 	</div>
