f3460da3793bd712d894228ca4121b7f1622ec8f
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-06-21 10:49:59 +0700

    perbaikan dan update

---
+++ member/histori.php
@@ -0,0 +1,15 @@
+<section>
+	<div class="col-sm-9" style="font-family:Arial;">
+		<div class="list-group">
+			<div class="panel" style="border:0px;">
+  				<div class="panel-heading" style="background-color:#FF5722">
+    				<h3 class="panel-title" style="font-weight:600; color:white; margin-top:10px; margin-bottom:10px;">HISTORI</h3>
+  				</div>
+	  					<br/>
+	  				    <div class="panel-body">
+
+		  				 </div>
+		  	</div>
+		</div>
+	</div>
+</section>
