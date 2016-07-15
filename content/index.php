2e38af6e20799120466d500ea5c81029dd027599
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-07-15 09:37:35 +0700

    perbaikan san update

--- content/index.php
+++ content/index.php
@@ -5,7 +5,7 @@
                 <h2 style="font-size:27px;line-height:35px;color:#fff;">Enjoy a High Speed Internet Access<br/>To Your Home Now!</h2>
                 <h6 style="font-size:17px;color:#555;font-weight:400;color:#fff;">Combine high speed Internet access with live TV to get more fun.</h6><br/>
                 <button data-toggle="modal" data-target="#signupModal" class="buttonOrange">Get Started</button>&nbsp;
-                <a href="<?php echo $base_url; ?>/?hal=features" style="background-color:rgba(255, 255, 255, 0);border:2px solid #f9a825;color:#f9a825;height:40px;padding:12px 25px 12px 25px;border-radius:3px;font-weight:bold;">Learn More</a>&nbsp;&nbsp;
+                <a href="<?php echo $base_url; ?>/features" style="background-color:rgba(255, 255, 255, 0);border:2px solid #f9a825;color:#f9a825;height:40px;padding:12px 25px 12px 25px;border-radius:3px;font-weight:bold;">Learn More</a>&nbsp;&nbsp;
             </div>
             <div class="col-sm-6 hidden-xs">
                 <img class="hidden-xs" src="<?php echo $base_url; ?>/img/groovy-multi-device.png" style="width:90%;margin-top:50px;"/>
@@ -13,7 +13,7 @@
         </div>
     </div>
     <!-- /Content1 -->
-    
+
     <!-- Content2 -->
     <div style="background-color:#F2F2F2;background-image:url(<?php echo $base_url; ?>/img/high-speed-fiber-internet.png);background-repeat:no-repeat;background-position:right top;">
         <div class="container" style="padding: 15px 20px 15px 20px;">
@@ -23,11 +23,11 @@
                     <p style="font-size:18px;">With Groovy High-Speed Fiber Internet, you're always connected to our reliable network.</p>
                 </div>
             </div>
-            
-        </div> 
+
+        </div>
     </div>
     <!-- /Content3 -->
-    
+
     <!-- Content3 -->
     <div style="background-color:#000;background-image:url(<?php echo $base_url; ?>/img/all-entertainment.jpg);background-size:cover;">
         <div class="container" style="padding: 15px 20px 10px 20px;">
@@ -37,11 +37,11 @@
                     <p style="font-size:18px;color:#fff;">You can take your entertainment with you. Watch the hottest shows and movies On Demand, stream live TV, social TV or watch your recorded shows. Just turn on your TV, laptop, tablet or phone.</p>
                 </div>
             </div>
-            
-        </div> 
+
+        </div>
     </div>
     <!-- /Content3 -->
-    
+
     <!-- Content 4 -->
     <div style="background: linear-gradient(to right, #f9a825 , #fdd835);color:#000;padding:60px 0 80px 0;">
         <div class="container">
@@ -50,7 +50,7 @@
                     <h3 style="font-weight:200;">Go with Groovy High-Speed Fiber Internet for a consistently fast connection.</h3>
                 </div>
                 <div class="col-sm-3" style="padding:5px 0 0 18px;">
-                    <button class="navButton" style="padding:10px 50px 10px 50px;color:#fff;font-size:18px;">Register Now</button>    
+                    <button class="navButton" style="padding:10px 50px 10px 50px;color:#fff;font-size:18px;">Register Now</button>
                 </div>
             </div>
         </div>
