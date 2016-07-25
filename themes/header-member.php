83c191f6a94a3725ddf396bed3dbde083b2e5f14
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-07-25 14:46:27 +0700

    perbaikan dan update

--- themes/header-member.php
+++ themes/header-member.php
@@ -351,8 +351,3 @@ if($emailaktivasi && $update_user && $kirim_email1 && $push_histori){ ?>
     </div>
     </form>
   </div>
-  <div class="alert alert-dismissible alert-warning">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <h4>Warning!</h4>
-    <p>Best check yo self, you're not looking too good, <a href="#" class="alert-link">vel scelerisque nisl consectetur et</a>.</p>
-  </div>
