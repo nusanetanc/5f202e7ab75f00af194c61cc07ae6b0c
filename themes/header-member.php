41bab15fb0df4068ddee4e3a102c62b9be251c63
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2016-08-01 10:55:29 +0700

    perbaikan dan update by yudi

--- themes/header-member.php
+++ themes/header-member.php
@@ -90,7 +90,7 @@ if($level=="501"){
       ?>
       <ul class="nav navbar-nav">
         <li ><a href="<?php echo $base_url_member; ?>/<?php echo $row['file']; ?>"  style="font-size:14px;padding-top:20px;padding-bottom:19px;font-weight:500; color:gray;"><i style="padding-right:10px;" class="fa fa-<?php echo $row['image']; ?> fa-lg"></i><?php echo $row['title'].' '; ?>
-          <?php if($row['title'] == "information"){ ?>
+          <?php if($row['title'] == "INFORMATION"){ ?>
             <span class="badge">3</span>
           <?php }  ?>
         </a></li>
